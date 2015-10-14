6b78a237a62a37be0896f61d182b11aa0e870d72
wreckingadm <wreckingadm@users.noreply.github.com>
2015-10-14 00:01:57 -0400

    Initial blink support
    Created a new tag <blink-> that we can play with, but am shifting to a
    more direct approach that makes more sense.

--- blinkagain.js
+++ blinkagain.js
@@ -6,3 +6,32 @@
  * Copyright 2015 BOXdev.com, wreckreation.org
  */
 
+var BlinkAgainProto = Object.create( HTMLElement.prototype );
+
+Object.defineProperty( BlinkAgainProto, 'rate', { 
+    value: 20,
+    writable : true
+});
+
+Object.defineProperty( BlinkAgainProto, 'smooth', { 
+    value: false,
+    writable : true
+});
+
+var BlinkAgainElement = document.registerElement( 'blink-',  { 
+    prototype: BlinkAgainProto
+});
+
+$( document ).ready( function()
+{
+   blink();
+});
+
+function blink() {
+   setTimeout( function()
+   {
+      //TODO: toggle() won't work because it removes the DOM from the flow
+      $( 'blink-' ).toggle();
+      blink();
+   }, 1000 );
+}
