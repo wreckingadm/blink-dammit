92c74ef99fa1912f9beeb11c9d687a28f1333a7a
wreckingadm <wreckingadm@users.noreply.github.com>
2015-10-13 23:20:57 -0400

    Initial empty files
    Copying and emptying from another repo for the time being. CSS is
    placeholder.

---
+++ blinkagain.js
@@ -0,0 +1,8 @@
+/*!
+ * <blink>again - 
+ * Do you miss <blink> as much as I do? Yeah, I thought so.
+ * https://github.com/wreckingadm/blinkagain
+ *
+ * Copyright 2015 BOXdev.com, wreckreation.org
+ */
+
